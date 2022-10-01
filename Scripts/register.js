56f23b2368823d9b331fb315e56a744bde4c0e7f
rajkumar-cmd <raymarhossor@gmail.com>
2022-10-01 15:21:31 +0530

    js created

---
+++ Scripts/register.js
@@ -0,0 +1,16 @@
+document.querySelector("form").addEventListener("submit",regApply);
+
+let regArr=JSON.parse(localStorage.getItem("regArr"))||[];
+
+function regApply(event){
+    event.preventDefault();
+    console.log("hi");
+    let regObj={
+        personal_name:document.querySelector("#name").value,
+        personal_email:document.querySelector("#email").value,
+        personal_password:document.querySelector("#password").value,
+        personal_mobno:document.querySelector('#mobno').value
+    };
+    regArr.push(regObj);
+    localStorage.setItem("regDetail",JSON.stringify(regArr));
+}
